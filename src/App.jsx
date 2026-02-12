189494f70e255d6cbea8a4665ae96f0fd8de52f1
gilliard09 <jefersonrocha998@gmail.com>
2026-02-12 19:13:28 -0300

    Ajuste final de caminhos e nomes de arquivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 // Importação das suas páginas
 import Dashboard from './pages/Dashboard';
 import Editor from './pages/Editor';
 import Leitura from './pages/Leitura';
-import Perfil from './pages/perfil'; // Página que criamos agora
+import Perfil from './pages/Perfil';
+import Devocionais from './pages/Devocionais';
 
 // Importação do componente de navegação
 import Tabs from './components/Tabs';
@@ -13,7 +13,10 @@ import Tabs from './components/Tabs';
 function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-gray-50">
+      <div className="min-h-screen bg-gray-50 flex flex-col">
+        {/* O componente Tabs fica no topo para aparecer fixo em todas as telas */}
+        <Tabs />
+        
         {/* Área de conteúdo que muda conforme a rota */}
         <Routes>
           {/* Rota Principal: Lista de Sermões */}
@@ -29,14 +32,9 @@ function App() {
           {/* Rota do Perfil do Pastor */}
           <Route path="/perfil" element={<Perfil />} />
           
-          {/* Rota para Devocionais (que faremos a seguir) */}
-          <Route path="/devocionais" element={<div className="p-10 text-center">Em breve: Devocionais</div>} />
+          {/* Rota para Devocionais */}
+          <Route path="/devocionais" element={<Devocionais />} />
         </Routes>
-
-        {/* O componente Tabs fica fora das Routes para que 
-          ele apareça fixo em todas as telas principais 
-        */}
-        <Tabs />
       </div>
     </BrowserRouter>
   );
